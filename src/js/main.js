18fd6083d65db30401961642e0edff197637a915
myslikam <ammyslik@gmail.com>
2020-03-21 12:43:20 +0300

    21.03_12Ж43

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/* do {
+var num1 = +prompt ('Введите число 1');
+var num2 = +prompt ('Введите число 2');
+var result = num1 + num2;
+alert("Резельтат:" + result);
+var isProcess = confirm ('Продолжить');
+} while (isProcess) */
