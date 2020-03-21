6ce90a7c9b172e88d3a3a406a634d4402f61ea3c
myslikam <ammyslik@gmail.com>
2020-03-21 12:55:08 +0300

    21.03_15:55

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,22 @@ var result = num1 + num2;
 alert("Резельтат:" + result);
 var isProcess = confirm ('Продолжить');
 } while (isProcess) */
+
+function getValues () {
+var result = [];
+var i = 0;
+do {
+    result[i] = +prompt ('Введите число');
+    i++
+    var isProcess = confirm ('Продолжить');
+}while (isProcess);
+
+return result;
+
+}
+
+do {
+    var numbers = getValues();
+    alert(numbers);
+    var isProcess = confirm ('Продолжить');
+}while (isProcess);
